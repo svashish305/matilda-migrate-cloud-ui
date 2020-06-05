aaa8e67923a0780b8e71ce52e7db3a54b2abdc0a
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-06-05 17:14:36 +0530

    added modules for test component

--- src/app/modules/test/test.module.ts
+++ src/app/modules/test/test.module.ts
@@ -1,15 +1,33 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { TestRoutingModule } from './test-routing.module';
-import { TestComponent } from './test.component';
-
+import { TestRoutingModule } from "./test-routing.module";
+import { TestComponent } from "./test.component";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { AvatarModule } from "ngx-avatar";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MaterialModule } from "../material.module";
+import { SearchPipeModule } from "../searchpipe.module";
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { ResizableModule } from "angular-resizable-element";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [TestComponent],
   imports: [
     CommonModule,
-    TestRoutingModule
-  ]
+    TestRoutingModule,
+    HttpClientModule,
+    RouterModule,
+    AvatarModule,
+    FormsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    SearchPipeModule,
+    DragDropModule,
+    ResizableModule,
+    NgbModule,
+  ],
 })
-export class TestModule { }
+export class TestModule {}
