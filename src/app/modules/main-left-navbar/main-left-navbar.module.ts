b278a2ec240cb3f9e3163101e5b2cb41d96189bf
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-06-08 15:39:53 +0530

    minor WIP

--- src/app/modules/main-left-navbar/main-left-navbar.module.ts
+++ src/app/modules/main-left-navbar/main-left-navbar.module.ts
@@ -4,10 +4,20 @@ import { MainLeftNavbarComponent } from "./main-left-navbar.component";
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { MaterialModule } from "../material.module";
+import { TemplatesModule } from "../templates/templates.module";
+import { WorkflowsModule } from "../workflows/workflows.module";
+import { HubModule } from "../hub/hub.module";
 
 @NgModule({
   declarations: [MainLeftNavbarComponent],
-  imports: [CommonModule, RouterModule, MaterialModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MaterialModule,
+    // HubModule,
+    // TemplatesModule,
+    // WorkflowsModule,
+  ],
   exports: [MainLeftNavbarComponent],
   providers: [],
   bootstrap: [],
