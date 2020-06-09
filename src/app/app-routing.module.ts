47d36bd46c509720222a389ca6bdaba484f2d3d0
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-06-09 12:00:58 +0530

    fixed drag drop styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,45 +1,58 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./modules/home/home.component";
 
 const routes: Routes = [
   {
     path: "",
-    loadChildren: () =>
-      import("./modules/home/home.module").then((m) => m.HomeModule),
-    data: { breadcrumbItem: { key: "home", labelName: "Home" }, title: "Home" },
-  },
-  {
-    path: "hub",
-    loadChildren: () =>
-      import("./modules/hub/hub.module").then((m) => m.HubModule),
-    data: { breadcrumbItem: { key: "hub", labelName: "Hub" }, title: "Hub" },
-  },
-  {
-    path: "templates",
-    loadChildren: () =>
-      import("./modules/templates/templates.module").then(
-        (m) => m.TemplatesModule
-      ),
-    data: {
-      breadcrumbItem: { key: "templates", labelName: "Templates" },
-      title: "Templates",
-    },
-  },
-  {
-    path: "workflows",
-    loadChildren: () =>
-      import("./modules/workflows/workflows.module").then(
-        (m) => m.WorkflowsModule
-      ),
-    data: {
-      breadcrumbItem: { key: "workflows", labelName: "Workflows" },
-      title: "Workflows",
-    },
-  },
-  {
-    path: "test",
-    loadChildren: () =>
-      import("./modules/test/test.module").then((m) => m.TestModule),
+    component: HomeComponent,
+    children: [
+      // {
+      //   path: "",
+      //   loadChildren: () =>
+      //     import("./modules/home/home.module").then((m) => m.HomeModule),
+      //   data: {
+      //     breadcrumbItem: { key: "home", labelName: "Home" },
+      //     title: "Home",
+      //   },
+      // },
+      {
+        path: "hub",
+        loadChildren: () =>
+          import("./modules/hub/hub.module").then((m) => m.HubModule),
+        data: {
+          breadcrumbItem: { key: "hub", labelName: "Hub" },
+          title: "Hub",
+        },
+      },
+      {
+        path: "templates",
+        loadChildren: () =>
+          import("./modules/templates/templates.module").then(
+            (m) => m.TemplatesModule
+          ),
+        data: {
+          breadcrumbItem: { key: "templates", labelName: "Templates" },
+          title: "Templates",
+        },
+      },
+      {
+        path: "workflows",
+        loadChildren: () =>
+          import("./modules/workflows/workflows.module").then(
+            (m) => m.WorkflowsModule
+          ),
+        data: {
+          breadcrumbItem: { key: "workflows", labelName: "Workflows" },
+          title: "Workflows",
+        },
+      },
+      {
+        path: "test",
+        loadChildren: () =>
+          import("./modules/test/test.module").then((m) => m.TestModule),
+      },
+    ],
   },
 ];
 
