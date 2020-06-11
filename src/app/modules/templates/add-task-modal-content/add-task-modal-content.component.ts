59a87737a4a8fab0f2386c9facab03be14825a8d
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-06-11 10:03:33 +0530

    fixed frag icon displacement bug

--- src/app/modules/templates/add-task-modal-content/add-task-modal-content.component.ts
+++ src/app/modules/templates/add-task-modal-content/add-task-modal-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 interface SelectInterface {
