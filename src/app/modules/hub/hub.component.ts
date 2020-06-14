c3ff80b63de4e939f5bdf95bf02a5635d3a37cb5
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-06-14 17:55:55 +0530

    hub styles WIP

--- src/app/modules/hub/hub.component.ts
+++ src/app/modules/hub/hub.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./hub.component.scss"],
 })
 export class HubComponent implements OnInit {
+  searchKey;
+  isTemplateFavouritesCollapsed: true;
+  isWorkflowFavouritesCollapsed: false;
+
   constructor() {}
 
   ngOnInit() {}
+
+  /**
+   *
+   * @description searches the wavelist using the search key
+   */
+
+  search(e) {
+    // if (!this.searchKey) {
+    //   this.templates = this.rawtemplates;
+    //   return true;
+    // }
+    // this.templates = this.rawtemplates.filter((x) => {
+    //   return x.name.toLowerCase().search(this.searchKey.toLowerCase()) !== -1;
+    // });
+  }
 }
--- src/app/modules/hub/hub.module.ts
+++ src/app/modules/hub/hub.module.ts
@@ -4,10 +4,27 @@ import { CommonModule } from "@angular/common";
 import { HubRoutingModule } from "./hub-routing.module";
 import { HubComponent } from "./hub.component";
 import { MaterialModule } from "../material.module";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AvatarModule } from "ngx-avatar";
+import { SearchPipeModule } from "../searchpipe.module";
+
+const avatarColors = ["#5fb8f1", "#012b7a"];
 
 @NgModule({
   declarations: [HubComponent],
-  imports: [CommonModule, HubRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    HubRoutingModule,
+    MaterialModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AvatarModule.forRoot({
+      colors: avatarColors,
+    }),
+    SearchPipeModule,
+  ],
   exports: [HubComponent],
 })
 export class HubModule {}
