2ad6c943e0c5a03f34e0e9cedc482bddf877ca93
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-06-17 12:34:14 +0530

    fixed routing

--- src/app/modules/main-left-navbar/main-left-navbar.component.ts
+++ src/app/modules/main-left-navbar/main-left-navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { DeviceDetectorService } from "ngx-device-detector";
+import { DataService } from "src/services/data.service";
+import * as uuid from "uuid";
 
 @Component({
   selector: "app-main-left-navbar",
@@ -18,7 +20,8 @@ export class MainLeftNavbarComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private deviceService: DeviceDetectorService
+    private deviceService: DeviceDetectorService,
+    private dataService: DataService
   ) {}
 
   ngOnInit() {
@@ -87,5 +90,182 @@ export class MainLeftNavbarComponent implements OnInit {
     return {
       borderRight: this.workflowSelected ? "3px solid white" : "none",
     };
+  }
+
+  addTemplate() {
+    const id = uuid.v4();
+    const newTemplate = {
+      id: id,
+      name: "Untitled Template",
+      data: {
+        id: id,
+        name: "Untitled Template",
+        description: "Default Description",
+        status: "Configured",
+        progressPercentage: 50,
+      },
+    };
+    this.dataService.addTemplate(newTemplate).subscribe((res: any) => {
+      this.router.navigate([`/templates/${res.id}`]);
+    });
+  }
+
+  addWorkflow() {
+    const id = uuid.v4();
+    const newWorkflow = {
+      id: id,
+      name: "Untitled Workflow",
+      data: {
+        name: "Untitled Workflow",
+        description: "Default Wave Description",
+        waveTypes: [
+          {
+            id: "1",
+            name: "Wave1-APP",
+            triggerType: "Normal",
+            status: "Configured",
+            progressPercentage: 50,
+            collapsed: false,
+            templates: [
+              {
+                id: 1,
+                name: "Tomcat",
+                description: "Tomcat Install Configuration",
+                data: {
+                  id: 1,
+                  name: "Tomcat",
+                  description: "Tomcat Install Configuration",
+                },
+                status: "Configured",
+                progressPercentage: 50,
+                startDate: "2 April 2020",
+                endDate: "14 April 2020",
+              },
+              {
+                id: 2,
+                name: "Sqlserver install",
+                description: "Install Configuration",
+                data: {
+                  id: 2,
+                  name: "Sqlserver install",
+                  description: "Install Configuration",
+                },
+                status: "Configured",
+                progressPercentage: 50,
+                startDate: "2 April 2020",
+                endDate: "14 April 2020",
+              },
+              {
+                id: 3,
+                name: "Template Name Application VM creation",
+                description: "Install Configuration",
+                data: {
+                  id: 3,
+                  name: "Template Name Application VM creation",
+                  description: "Install Configuration",
+                },
+                status: "Configured",
+                progressPercentage: 50,
+                startDate: "31 March 2020",
+                endDate: "9 April 2020",
+              },
+            ],
+          },
+          {
+            id: "2",
+            name: "Wave1-DB",
+            status: "Configured",
+            progressPercentage: 50,
+            collapsed: false,
+            triggerType: "Event",
+            templates: [
+              {
+                id: 1,
+                name: "Data factory",
+                description: "Factory Install Steps",
+                data: {
+                  id: 1,
+                  name: "Data factory",
+                  description: "Factory Install Steps",
+                },
+                status: "Configured",
+                progressPercentage: 50,
+                startDate: "4 April 2020",
+                endDate: "20 April 2020",
+              },
+              {
+                id: 2,
+                name: "Data Warehouse configuration",
+                description: "DB Install Configuration",
+                data: {
+                  id: 2,
+                  name: "Data Warehouse configuration",
+                  description: "DB Install Configuration",
+                },
+                status: "Configured",
+                progressPercentage: 50,
+                startDate: "2 April 2020",
+                endDate: "15 April 2020",
+              },
+            ],
+          },
+          {
+            id: "3",
+            name: "Wave1-Testing",
+            status: "Configured",
+            progressPercentage: 50,
+            collapsed: false,
+            triggerType: "Schedule | Every Monday 2 PM IST",
+            templates: [
+              {
+                id: 1,
+                name: "Post deployment validation",
+                status: "Success",
+                progressPercentage: 100,
+                startDate: "Apr 15",
+                endDate: "Apr 15",
+              },
+              {
+                id: 2,
+                name: "Security Scan infra",
+                status: "Failed",
+                progressPercentage: 0,
+                startDate: "",
+                endDate: "",
+              },
+            ],
+          },
+          {
+            id: "4",
+            name: "Wave1-Testing2",
+            status: "Configured",
+            progressPercentage: 50,
+            collapsed: false,
+            triggerType: "Normal",
+            templates: [
+              {
+                id: 1,
+                name: "Post deployment validation",
+                status: "Configured",
+                progressPercentage: 50,
+                startDate: "Apr 15",
+                endDate: "Apr 15",
+              },
+              {
+                id: 2,
+                name: "Security Scan infra",
+                status: "Configured",
+                progressPercentage: 50,
+                startDate: "",
+                endDate: "",
+              },
+            ],
+          },
+        ],
+      },
+    };
+    this.dataService.addWave(newWorkflow).subscribe((res: any) => {
+      this.router.navigate([`/workflows/${res.id}`]);
+    });
   }
 }
