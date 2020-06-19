0b6ab10a62f721414b5edceb0d79bd60319b2520
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-06-19 15:56:51 +0530

    added browser-level-scroll

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+declare var $: any;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
