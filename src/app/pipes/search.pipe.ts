f3a3f4a0b01759717c973a2d01bb19bd7e46fd47
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-06-24 12:21:24 +0530

    search works for sidebars

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -1,44 +1,15 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'search',
+  name: "search",
 })
 export class SearchPipe implements PipeTransform {
   transform(value: any, args?: any): any {
-    const search = args ? args : '';
-    if (search) {
-      let newData = [];
-      value.forEach((row, index) => {
-        newData.push({});
-        Object.keys(row).forEach((key) => {
-          if (Array.isArray(row[key])) {
-            newData[index][key] = [];
-            row[key].forEach((inner) => {
-              const obj = {};
-              Object.keys(inner).forEach((innerKey) => {
-                obj[innerKey] = inner[innerKey];
-              });
-              newData[index][key].push(obj);
-            });
-          } else {
-            newData[index][key] = row[key];
-          }
-        });
-      });
-      newData = newData.filter((waveType, index) => {
-        const fields = ['name', 'status', 'startDate', 'endDate'];
-        newData[index].templates = waveType.templates.filter((template) => {
-          return fields.some((x) => {
-            const ele = template[x].toLowerCase();
-            return ele.indexOf(search.toLowerCase()) !== -1;
-          });
-        });
-        return newData[index].templates && newData[index].templates.length;
-      });
-      return newData;
-    } else {
-      return value;
-    }
-    // return null;
+    if (!value) return null;
+    if (!args) return value;
+    args = args.toLowerCase();
+    return value.filter(function (item) {
+      return JSON.stringify(item).toLowerCase().includes(args);
+    });
   }
 }
