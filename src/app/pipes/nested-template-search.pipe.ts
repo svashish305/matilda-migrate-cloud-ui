17fe9de9080f5ef05a98cec44f0ae272d61893cf
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-06-25 14:12:11 +0530

    minor style fixes

--- src/app/pipes/nested-template-search.pipe.ts
+++ src/app/pipes/nested-template-search.pipe.ts
@@ -4,6 +4,34 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "nestedTemplateSearch",
 })
 export class NestedTemplateSearchPipe implements PipeTransform {
+  // transform(value: any, searchText?: any): any {
+  //   if (!value) return [];
+  //   if (!searchText) return value;
+  //   searchText = searchText.toLowerCase();
+  //   return value.filter((it) => {
+  //     // console.log(it);
+  //     let foundNested = false;
+  //     it.items.filter((item) => {
+  //       if (
+  //         item.name.toLowerCase().includes(searchText) ||
+  //         item.statusCd.toLowerCase().includes(searchText) ||
+  //         item.progress.toString().includes(searchText) ||
+  //         item.pluginName.toString().includes(searchText) ||
+  //         item.startDate.toString().includes(searchText) ||
+  //         item.endDate.toString().includes(searchText)
+  //       ) {
+  //         foundNested = true;
+  //       }
+  //     });
+  //     return (
+  //       it.name.toLowerCase().includes(searchText) ||
+  //       it.statusCd.toLowerCase().includes(searchText) ||
+  //       it.progress.toString().includes(searchText) ||
+  //       foundNested
+  //     );
+  //   });
+  // }
+
   transform(value: any, args?: any): any {
     let search = args ? args : "";
     if (search) {
