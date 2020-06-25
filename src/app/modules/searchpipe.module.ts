6240492c2d1926078a93fd8fb33bbbacb83a978a
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-06-25 20:20:43 +0530

    minor fixes

--- src/app/modules/searchpipe.module.ts
+++ src/app/modules/searchpipe.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from "@angular/common";
 import { SearchPipe } from "../pipes/search.pipe";
 import { NestedTemplateSearchPipe } from "../pipes/nested-template-search.pipe";
 import { NestedWorkflowSearchPipe } from "../pipes/nested-workflow-search.pipe";
+import { NaiveSearchPipe } from "../pipes/naive-search.pipe";
 
 @NgModule({
   declarations: [
     SearchPipe,
+    NaiveSearchPipe,
     NestedTemplateSearchPipe,
     NestedWorkflowSearchPipe,
   ],
   imports: [CommonModule],
-  exports: [SearchPipe, NestedTemplateSearchPipe, NestedWorkflowSearchPipe],
+  exports: [
+    SearchPipe,
+    NaiveSearchPipe,
+    NestedTemplateSearchPipe,
+    NestedWorkflowSearchPipe,
+  ],
   providers: [],
   bootstrap: [],
 })
