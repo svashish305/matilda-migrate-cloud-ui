ad5c5166441006c5c161bae9f0425a8ca1ae23bf
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-07-06 16:02:19 +0530

    fixed search, works as expected

--- src/app/pipes/nested-template-search.pipe.ts
+++ src/app/pipes/nested-template-search.pipe.ts
@@ -4,85 +4,10 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "nestedTemplateSearch",
 })
 export class NestedTemplateSearchPipe implements PipeTransform {
-  // transform(value: any, searchText?: any): any {
-  //   if (!value) return [];
-  //   if (!searchText) return value;
-  //   searchText = searchText.toLowerCase();
-  //   return value.filter((it) => {
-  //     // console.log(it);
-  //     let foundNested = false;
-  //     it.items.filter((item) => {
-  //       if (
-  //         item.name.toLowerCase().includes(searchText) ||
-  //         item.status.toLowerCase().includes(searchText) ||
-  //         item.progress.toString().includes(searchText) ||
-  //         item.pluginName.toString().includes(searchText) ||
-  //         item.startDate.toString().includes(searchText) ||
-  //         item.endDate.toString().includes(searchText)
-  //       ) {
-  //         foundNested = true;
-  //       }
-  //     });
-  //     return (
-  //       it.name.toLowerCase().includes(searchText) ||
-  //       it.status.toLowerCase().includes(searchText) ||
-  //       it.progress.toString().includes(searchText) ||
-  //       foundNested
-  //     );
-  //   });
-  // }
-
   transform(value: any, args?: any): any {
-    let search = args ? args : "";
-    // if (search) {
-    //   let newData = [];
-    //   value.forEach((row, index) => {
-    //     newData.push({});
-    //     Object.keys(row).forEach((key) => {
-    //       if (Array.isArray(row[key])) {
-    //         newData[index][key] = [];
-    //         row[key].forEach((inner) => {
-    //           let obj = {};
-    //           Object.keys(inner).forEach((innerKey) => {
-    //             obj[innerKey] = inner[innerKey];
-    //           });
-    //           newData[index][key].push(obj);
-    //         });
-    //       } else {
-    //         newData[index][key] = row[key];
-    //       }
-    //     });
-    //   });
-    //   newData = newData.filter((templateType, index) => {
-    //     if (templateType.name.includes(search.toLowerCase())) {
-    //       return newData[index];
-    //     } else {
-    //       let fields = [
-    //         "name",
-    //         "status",
-    //         "progress",
-    //         "pluginName",
-    //         "startDate",
-    //         "endDate",fff
-    //       ];
-    //       newData[index].items = templateType.items.filter((item) => {
-    //         return fields.some((x) => {
-    //           let ele = item[x].toString().toLowerCase();
-    //           // console.log("ele ", ele);
-    //           return ele.indexOf(search.toLowerCase()) != -1;
-    //         });
-    //       });
-    //       return newData[index].items && newData[index].items.length;
-    //     }
-    //   });
-    //   return newData;
-    // } else {
-    //   return value;
-    // }
-    // return null;
-
-    let rootFields = ["name", "status", "progress"];
-    let childFields = [
+    const search = args ? args : "";
+    const rootFields = ["name", "status", "progress"];
+    const childFields = [
       "name",
       "pluginName",
       "status",
@@ -92,44 +17,46 @@ export class NestedTemplateSearchPipe implements PipeTransform {
     ];
     let rootFound = false;
     let childFound = false;
-    let groups = [];
-    value.forEach((group) => {
-      for (let i = 0; i < rootFields.length; i++) {
+    const groups = [];
+    const tempValue = value.map((x: any) => Object.assign({}, x));
+    tempValue.forEach((group) => {
+      rootFound = false;
+      const items = [];
+      rootFields.forEach((rootFieldKey) => {
         if (
-          group[rootFields[i]]
+          !rootFound &&
+          typeof group[rootFieldKey] !== "object" &&
+          group[rootFieldKey]
             .toString()
             .toLowerCase()
             .includes(search.toLowerCase())
         ) {
           rootFound = true;
-        } else {
-          let items = [];
-          group.items.forEach((item) => {
-            for (let i = 0; i < childFields.length; i++) {
-              if (
-                item[childFields[i]]
-                  .toString()
-                  .toLowerCase()
-                  .includes(search.toLowerCase())
-              ) {
-                childFound = true;
-              }
-            }
-            if (childFound) {
+          groups.push(group);
+        }
+      });
+      if (!rootFound) {
+        group.items.forEach((item) => {
+          childFound = false;
+          childFields.forEach((childFieldKey) => {
+            if (
+              !childFound &&
+              item[childFieldKey]
+                .toString()
+                .toLowerCase()
+                .includes(search.toLowerCase())
+            ) {
+              childFound = true;
               items.push(item);
-              childFound = false;
             }
           });
+        });
+        if (items.length) {
           group.items = items;
+          groups.push(group);
         }
       }
-      if (rootFound || childFound) {
-        groups.push(group);
-        rootFound = false;
-        childFound = false;
-      }
     });
-    value = groups;
-    return value;
+    return groups;
   }
 }
