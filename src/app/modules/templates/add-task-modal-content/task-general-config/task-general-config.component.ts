b3c7c981c28f681c5fb6aecbec60b3992e2b06a3
Uday <Uday@cnet-global.com>
2020-07-06 18:04:16 -0500

    Validation fix in General Tab

--- src/app/modules/templates/add-task-modal-content/task-general-config/task-general-config.component.ts
+++ src/app/modules/templates/add-task-modal-content/task-general-config/task-general-config.component.ts
@@ -49,7 +49,8 @@ export class TaskGeneralConfigComponent implements OnInit {
   }
 
   onPluginChange(event: any) {
-    
+    this.service.reset();
+    this.action.reset();
     this._pluginName = event.source.selected.viewValue;
     const filterPluginList = this.pluginList.filter(_plugin => _plugin.pluginId === event.value);
     console.log(filterPluginList);
@@ -57,20 +58,23 @@ export class TaskGeneralConfigComponent implements OnInit {
       this.showServiceControl = true;
       this.serviceList = filterPluginList[0].pluginServices;
       this.service.setValidators([Validators.required]);
+      this.service.updateValueAndValidity();
     }else{
       this.showServiceControl = false;
       this.serviceList = [];
       this.service.setValidators([Validators.nullValidator]);
+      this.service.updateValueAndValidity();
       if(filterPluginList[0].pluginActions) {
         this.showActionControl = true;
         this.actionList = filterPluginList[0].pluginActions;
         this.action.setValidators([Validators.required]);
+        this.action.updateValueAndValidity();
       }
     }
   }
 
   onServiceChange(event: any, pluginId: any) { 
-    this.action.patchValue(null);
+    this.action.reset();
     this._serviceName = event.source.selected.viewValue;
       this.pluginList.forEach(_plugin => {
         if(_plugin.pluginId === pluginId) {
@@ -79,6 +83,7 @@ export class TaskGeneralConfigComponent implements OnInit {
               this.showActionControl = true;
               this.actionList = _service.pluginActions;
               this.action.setValidators([Validators.required]);
+              this.action.updateValueAndValidity();
             }
           });
         }
--- src/app/modules/templates/add-task-modal-content/task-output/task-output.component.ts
+++ src/app/modules/templates/add-task-modal-content/task-output/task-output.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-task-output',
   templateUrl: './task-output.component.html',
-  styleUrls: ['./task-output.component.scss']
+  styleUrls: ['./task-output.component.scss'],
+  //encapsulation: ViewEncapsulation.None
 })
 export class TaskOutputComponent implements OnInit {
   @Input() task: any
