cbce9c8b22185a6acfef997a9a7a4483f27aa8d3
Shubhashish Verma <svashish305@gmail.com>
2020-07-09 18:04:46 +0530

    added discover component and routes and set up breadcrumb

---
+++ src/app/modules/templates/template-discover/template-discover.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-discover',
+  templateUrl: './template-discover.component.html',
+  styleUrls: ['./template-discover.component.scss']
+})
+export class TemplateDiscoverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
